88cda01a81a6caeea3790b8c2a64fdc368d3aeef
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-10-20 15:39:26 +0300

    add post and comment models

---
+++ db/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, Boolean, DateTime, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from db.engine import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    date_time_created = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    is_blocked = Column(Boolean, default=False)
+    auto_reply = Column(Boolean, default=False)
+    auto_reply_time = Column(Integer, default=0)
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    date_time_created = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    is_blocked = Column(Boolean, default=False)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+
+    post = relationship(Post)
+
